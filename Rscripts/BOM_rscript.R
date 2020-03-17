4b60e863cb89c76616c8827f33c0d49274278332
McEnnulty <mce031@csiro.au>
2020-03-17 12:15:56 +1100

    separate, filter, group_by, summarise BOM_data file to tidy it up

--- Rscripts/BOM_rscript.R
+++ Rscripts/BOM_rscript.R
@@ -12,6 +12,46 @@ library(tidyverse)
 BOM_data <- read_csv("Data/BOM_data.csv")
 BOM_stations <- read_csv("Data/BOM_stations.csv")
 
+#show data
+BOM_data
+head(BOM_data)
+tail(BOM_data)
+summary(BOM_data)
+
+#Question 1
+#For each station, how many days have a minimum temperature, 
+#a maximum temperature and a rainfall measurement recorded?
+
+
+##separate
+
+#need to separate temp_min_max "12/22"
+?separate
+BOM_separated <- BOM_data %>% 
+                separate(Temp_min_max, into = c("Min_temp", "Max_temp"),
+                          sep = "/")
+BOM_separated
+
+##filter
+
+# filter() the data to keep only rows that have minimum temperature, 
+#maximum temperature, and rainfall measurements.
+#filter(gapminder, country == "Australia", year >= 1997)
+BOM_filtered <- BOM_separated %>%
+                filter(Min_temp != "-", Max_temp != "-", Rainfall != "-")
+
+#A group_by() followed by summarise() will then allow you to 
+#count the number of rows remaining for each station.
+
+#group by station
+BOM_grouped <- BOM_filtered %>% 
+                group_by(Station_number) 
+#see tibble header - there are 20 stations
+
+BOMbyStn_countdays <- BOM_grouped%>%summarise(n_days = n())
+
+#to nest these so you don't make lots of dataframes
+
 
 #For each station, how many days have a minimum temperature, 
 #a maximum temperature and a rainfall measurement recorded?
